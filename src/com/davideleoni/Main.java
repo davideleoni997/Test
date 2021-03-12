ed6530c103f7f38b08f0124b30b4e6e16871e5b9
davideleoni997 <leonidavide1997@gmail.com>
2021-03-12 10:33:50 +0100

    Modified logger

--- src/com/davideleoni/Main.java
+++ src/com/davideleoni/Main.java
@@ -1,8 +1,11 @@
 package com.davideleoni;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello"); //stampa
+	Logger.getGlobal().log(Level.FINE,"Hello World"); //stampa
     }
 }
